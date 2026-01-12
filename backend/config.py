53a30cbb90fe4fb7c1505ac90e785fb45be35cde
vihaanpasole-hub <vihaanpasole@gmail.com>
2026-01-12 18:32:44 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 DB_PATH = os.path.join(BASE_DIR, "..", "instance", "database.db")
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
